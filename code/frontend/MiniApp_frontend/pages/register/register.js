4c7989641b7336ee63c0ed17eec861d7c5455672
hsc <hsc934936408@sjtu.edu.cn>
2019-07-01 17:14:45 +0800

    finished technology tags

--- code/frontend/MiniApp_frontend/pages/register/register.js
+++ code/frontend/MiniApp_frontend/pages/register/register.js
@@ -1,4 +1,6 @@
 // pages/register/register.js
+const { $Toast } = require('../../dist/base/index');
+
 Page({
   /*
     可以从userInfo中获取：
@@ -10,30 +12,64 @@ Page({
   data: {
     technology: [
       {
+        id: 0,
+        name: "厨师",
+        isChosen: false
+      },
+      {
         id: 1,
-        name: "厨师"
+        name: "收银",
+        isChosen: false
       },
       {
         id: 2,
-        name: "收银"
+        name: "打杂",
+        isChosen: false
       },
       {
         id: 3,
-        name: "打杂"
+        name: "测试行超出会发生什么情况啦啦啦啦啦啦",
+        isChosen: false
       }
     ],
-    currentTechnology: [],
+    chosenTechnology: [],
     name: '',
     identity: '',
     phoneNumber: ''
   },
 
-  handleTechnologyChange({ detail = {} }) {
-    const index = this.data.currentTechnology.indexOf(detail.value);
-    index === -1 ? this.data.currentTechnology.push(detail.value) : this.data.currentTechnology.splice(index, 1);
+  chooseTechnology: function(e){
+    var newChosen = this.data.chosenTechnology;
+    var hasSame = false;
+    //判断已选技术中是否有重复的
+    for (var index in newChosen){
+      if (newChosen[index].id == e.detail.name)
+        hasSame = true;
+    }
+    if (hasSame == false) {
+      var toChosen = this.data.technology;
+      toChosen[e.detail.name].isChosen = true;
+      newChosen.push(this.data.technology[e.detail.name]);
+      this.setData({
+        chosenTechnology: newChosen,
+        technology: toChosen
+      })
+    }
+    else{
+      this.handleError();
+    }
+  },
+
+  deleteTechnology: function(e) {
+    var newChosen = this.data.chosenTechnology;
+    var toChosen = this.data.technology;
+    var switchIndex = newChosen[e.detail.name].id;
+    newChosen.splice(e.detail.name, 1);
+    toChosen[switchIndex].isChosen = false;
     this.setData({
-      currentTechnology: this.data.currentTechnology
-    });
+      chosenTechnology: newChosen,
+      technology: toChosen
+    })
   },
 
   getName: function(e){
@@ -54,6 +90,14 @@ Page({
     })
   },
 
+  handleError() {
+    $Toast({
+      content: '您已选择相同倾向',
+      type: 'error'
+    });
+  },
+
+  //向服务器发送请求
   Register: function () {
     
   }

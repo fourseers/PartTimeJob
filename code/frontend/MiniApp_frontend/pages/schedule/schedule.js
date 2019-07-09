d0a0d80a246b8dea9910d7a196cd2086143d2e5b
hsc <hsc934936408@sjtu.edu.cn>
2019-07-09 11:22:49 +0800

    enable add and delete schedule

--- code/frontend/MiniApp_frontend/pages/schedule/schedule.js
+++ code/frontend/MiniApp_frontend/pages/schedule/schedule.js
@@ -7,52 +7,91 @@ Page({
   data: {
     timeList: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
     schedule: [
-      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ],
+    scheduleSave: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ],
     week: [0, 1, 2, 3, 4, 5, 6],
-    pageWidth: 0.0
+    scheduleClass: ["cell", "cell choose-g", "scheduled-cell", "cell delete-g"],
+    newAdd: true,
+    tags: [
+      {
+        class: "color-item scheduled",
+        name: "已安排"
+      },
+      {
+        class: "color-item none",
+        name: "未安排"
+      },
+      {
+        class: "color-item add",
+        name: "待添加"
+      },
+      {
+        class: "color-item delete",
+        name: "待删除"
+      }
+    ]
   },
 
   onLoad(){
-    this.setData({
-      pageWidth: wx.getSystemInfoSync().windowWidth
-    });
-  },
 
-  handleScroll(e) {
-    console.log(e);
   },
 
   handleTap(e) {
-    console.log(e);
-  }
+    var newSchedule = this.data.scheduleSave;
+    //console.log(this.data.scheduleSave);
+    if(e.target.id !== ""){
+      var chosenId = parseInt(e.target.id);
+      var chosenDate = parseInt(e.target.id / 24);
+      var chosenTime = chosenId - chosenDate * 24;
+      if (newSchedule[chosenDate][chosenTime] === 0) {
+        newSchedule[chosenDate][chosenTime] = 1;
+      }
+      else if (newSchedule[chosenDate][chosenTime] === 1) {
+        newSchedule[chosenDate][chosenTime] = 0;
+      }
+      else if (newSchedule[chosenDate][chosenTime] === 2) {
+        newSchedule[chosenDate][chosenTime] = 3;
+      }
+      else if (newSchedule[chosenDate][chosenTime] === 3) {
+        newSchedule[chosenDate][chosenTime] = 2;
+      }
+      this.setData({
+        schedule: newSchedule,
+        newAdd: false
+      })
+    }
+  },
 
-/*
-  handleTouchStart(e){
-    var newData = this.data.listData;
-    var chosenIndex = parseInt(e.changedTouches[0].pageX / (this.data.pageWidth / 8));
-    //console.log(e.target.id !== "" && chosenIndex > 0);
-    if (e.target.id !== "" && chosenIndex > 0) {
-      newData[e.target.id][chosenIndex] = !newData[e.target.id][chosenIndex];
+  handleAdd() {
+    var newSchedule = this.data.schedule;
+    for (var i in newSchedule) {
+      for (var j in newSchedule[i]) {
+        if (newSchedule[i][j] == 1) {
+          newSchedule[i][j] = 2;
+        }
+        else if (newSchedule[i][j] == 3) {
+          newSchedule[i][j] = 0;
+        }
+      }
     }
     this.setData({
-      listData: newData
+      schedule: newSchedule,
+      newAdd: true
     })
-  },
-
-  handleTouchMove(e) {
-    console.log(e);
-  },
-
-  handleTouchEnd(e) {
-    console.log(e);
-  },
-
-*/
+  }
 })

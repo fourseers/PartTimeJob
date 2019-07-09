bcb94a758bc63a7f86e27adb2187e90944288c6e
hsc <hsc934936408@sjtu.edu.cn>
2019-07-09 09:53:14 +0800

    finished a simple schedule page

--- code/frontend/MiniApp_frontend/pages/schedule/schedule.js
+++ code/frontend/MiniApp_frontend/pages/schedule/schedule.js
@@ -6,6 +6,16 @@ Page({
    */
   data: {
     timeList: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+    schedule: [
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+    ],
+    week: [0, 1, 2, 3, 4, 5, 6],
     pageWidth: 0.0
   },
 
@@ -14,6 +24,14 @@ Page({
       pageWidth: wx.getSystemInfoSync().windowWidth
     });
   },
+
+  handleScroll(e) {
+    console.log(e);
+  },
+
+  handleTap(e) {
+    console.log(e);
+  }
 
 /*
   handleTouchStart(e){

3ac55967401402d7f4da44920bd382842971de1c
hsc <hsc934936408@sjtu.edu.cn>
2019-07-12 14:23:08 +0800

    finished a static table

---
+++ code/frontend/MiniApp_frontend/pages/timeline/timeline.js
@@ -0,0 +1,69 @@
+// pages/timeline/timeline.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    date: "周五",
+    week: ["周一", "周二", "周三", "周四", "周五", "周六", "周日",],
+    division: ["上午", "中午", "下午", "晚上"],
+    arrangement: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})

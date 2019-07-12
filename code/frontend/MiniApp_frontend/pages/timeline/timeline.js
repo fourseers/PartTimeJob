00aaebaa21b0a1fe31a7c1a51be20e1c16108681
hsc <hsc934936408@sjtu.edu.cn>
2019-07-12 15:15:35 +0800

    finish another kind of timeline

--- code/frontend/MiniApp_frontend/pages/timeline/timeline.js
+++ code/frontend/MiniApp_frontend/pages/timeline/timeline.js
@@ -3,67 +3,75 @@ Page({
 
   /**
    * 页面的初始数据
+   * arrangement 0未安排、1已安排、2待添加、3待删除
    */
   data: {
     date: "周五",
     week: ["周一", "周二", "周三", "周四", "周五", "周六", "周日",],
     division: ["上午", "中午", "下午", "晚上"],
     arrangement: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+    cell_classes: [
+      {
+        class: "cell unarranged unselected",
+        name: "未安排",
+        id: 0
+      },
+      {
+        class: "cell arranged selected",
+        name: "已安排",
+        id: 1
+      },
+      {
+        class: "cell unarranged selected",
+        name: "待添加",
+        id: 2
+      },
+      {
+        class: "cell arranged to-delete",
+        name: "待删除",
+        id: 3
+      },
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  handleTap(e){
+    //console.log(e);
+    var new_arrange = this.data.arrangement;
+    var date = e.currentTarget.id % 7;
+    var divide = parseInt(e.currentTarget.id / 7);
+    if (new_arrange[date][divide] == 0) {
+      new_arrange[date][divide] = 2;
+    }
+    else if (new_arrange[date][divide] == 2) {
+      new_arrange[date][divide] = 0;
+    }
+    else if (new_arrange[date][divide] == 1) {
+      new_arrange[date][divide] = 3;
+    }
+    else if (new_arrange[date][divide] == 3) {
+      new_arrange[date][divide] = 1;
+    }
 
+    this.setData({
+      arrangement: new_arrange
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleModify(){
+    var new_arrange = this.data.arrangement;
+    for(var date in this.data.arrangement){
+      for(var divide in this.data.arrangement[date]){
+        if (new_arrange[date][divide] == 2) {
+          new_arrange[date][divide] = 1;
+        }
+        if (new_arrange[date][divide] == 3) {
+          new_arrange[date][divide] = 0;
+        }
+      }
+    }
+    this.setData({
+      arrangement: new_arrange
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })

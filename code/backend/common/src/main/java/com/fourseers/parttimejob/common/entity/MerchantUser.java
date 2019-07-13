4edc8c4652cd33501d04c16c77c1e523ceabc915
eyeKill <1229522864@qq.com>
2019-07-13 12:08:38 +0800

    Resolve common library.

--- code/backend/common/src/main/java/com/fourseers/parttimejob/common/entity/MerchantUser.java
+++ code/backend/common/src/main/java/com/fourseers/parttimejob/common/entity/MerchantUser.java
@@ -3,7 +3,6 @@ package com.fourseers.parttimejob.common.entity;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
-
 @Entity
 public class MerchantUser {
 
--- code/backend/warehouse/src/main/java/com/fourseers/parttimejob/warehouse/repository/CompanyRepository.java
+++ code/backend/warehouse/src/main/java/com/fourseers/parttimejob/warehouse/repository/CompanyRepository.java
@@ -1,6 +1,7 @@
 package com.fourseers.parttimejob.warehouse.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import com.fourseers.parttimejob.common.entity.Company;
 

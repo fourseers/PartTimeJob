01dca27076a5fba5217192c9726a19a306a799b6
chris98122 <41262973+chris98122@users.noreply.github.com>
2019-07-17 17:03:22 +0800

    admin

---
+++ code/frontend/merchant_frontend/src/views/BanUser.vue
@@ -0,0 +1,73 @@
+<template>
+    <Content class="content">
+        <Table border :columns="columns7" :data="users"></Table>
+    </Content>
+</template>
+
+<script>
+    export default {
+        name: "BanUser",
+        data () {
+            return {
+                columns7: [
+                    {
+                        title: '用户名称',
+                        key: 'name',
+                        render: (h, params) => {
+                            return h('div', [
+                                h('strong', params.row.name)
+                            ]);
+                        }
+                    },
+                    {
+                        title: '用户状态',
+                        key: 'isbanned',
+                        render: (h,params) => {
+                            return h('i-switch',{
+                                props: {
+                                    size: 'large',
+                                    value: !params.row.isbanned
+                                },
+                                style: {
+                                    color: params.row.isbanned?"#68B2FE":'#d63031',
+                                    borderColor: params.row.isbanned?'#d63031':"#68B2FE",
+                                    backgroundColor: (params.row.isbanned)?'#d63031':"#68B2FE",
+                                },
+                                on: {
+                                    'on-change': (value) =>
+                                    {
+                                        console.log(value);
+                                        params.row.isbanned = !value;
+                                    }
+                                }
+                            }, [
+                                h('span', {
+                                    slot: 'open'
+                                }, '启用'),
+                                h('span', {
+                                    slot: 'close'
+                                }, '禁用')
+                            ])
+                        }
+                    }
+                ],
+                users:[
+                    {
+                        name:"www",
+                        isbanned:false
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .content{
+        margin-top:20px;
+        margin-left: 100px;
+        margin-right:50px;
+        background-color: #fff;
+    }
+</style>

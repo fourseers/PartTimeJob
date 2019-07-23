f8a09098e2b98d1021008748f984ad51fa4d3dc3
eyeKill <1229522864@qq.com>
2019-07-23 10:26:27 +0800

    fix missing manual_stop column in data.sql

--- code/backend/billing/src/test/resources/data.sql
+++ code/backend/billing/src/test/resources/data.sql
@@ -13,8 +13,8 @@ insert into shop (address, brand, city, industry_industry_id, introduction, lati
 ('510104', '锤子', '510100', 1, 'Aquire Apple in someday!', 30, 100, '510000', '锤科总部', 1);
 insert into wechat_user (city, country, education, gender, identity, name, openid, phone) values
 ('Shanghai', 'China', '高中毕业', 1, '310000000000000000', '苦逼打工仔', 'fake_openid', '13000000000');
-insert into job (begin_apply_time, begin_date, education, end_apply_time, end_date, begin_time, end_time, job_detail, job_name, need_amount, need_gender, salary, shop_shop_id) values
-('2019-07-14 16:00:00', '2019-07-16', '高中毕业', '2019-07-15 16:00:00', '2019-07-17', '08:00:00', '16:00:00', 'sell Chuizi products', 'seller', 10, 2, 100, 1);
+insert into job (begin_apply_time, begin_date, education, end_apply_time, end_date, begin_time, end_time, job_detail, job_name, need_amount, need_gender, salary, shop_shop_id, manual_stop) values
+('2019-07-14 16:00:00', '2019-07-16', '高中毕业', '2019-07-15 16:00:00', '2019-07-17', '08:00:00', '16:00:00', 'sell Chuizi products', 'seller', 10, 2, 100, 1, false);
 
 insert into work (checkin, checkout, log, salary_confirmed, score, work_date, billing_bill_id, job_job_id, worker_user_id) values
 ('08:00:00', '16:00:00', null, true, 5, '2019-07-17', null, 1, 1);
@@ -23,8 +23,8 @@ update work set billing_bill_id = 1 where work_id = 1;
 insert into work (checkin, checkout, log, salary_confirmed, score, work_date, billing_bill_id, job_job_id, worker_user_id) values
 ('08:00:00', '16:00:00', null, false, 5, '2019-07-18', null, 1, 1);
 
-insert into job (begin_apply_time, begin_date, education, end_apply_time, end_date, begin_time, end_time, job_detail, job_name, need_amount, need_gender, salary, shop_shop_id) values
-('2019-08-14 16:00:00', '2019-08-16', '高中毕业', '2019-08-15 16:00:00', '2019-08-17', '08:00:00', '16:00:00', 'sell Chuizi products', 'seller', 10, 2, 100, 1);
+insert into job (begin_apply_time, begin_date, education, end_apply_time, end_date, begin_time, end_time, job_detail, job_name, need_amount, need_gender, salary, shop_shop_id, manual_stop) values
+('2019-08-14 16:00:00', '2019-08-16', '高中毕业', '2019-08-15 16:00:00', '2019-08-17', '08:00:00', '16:00:00', 'sell Chuizi products', 'seller', 10, 2, 100, 1, false);
 insert into work (checkin, checkout, log, salary_confirmed, score, work_date, billing_bill_id, job_job_id, worker_user_id) values
 ('08:00:00', '16:00:00', null, true, 5, '2019-08-17', null, 2, 1);
 insert into billing (meta, method, payment, work_work_id) values (null, '微信支付', 100, 3);

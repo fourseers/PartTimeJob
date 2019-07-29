cb9f2b569ed7596b6c10717d961ac40b8c726f02
hsc <hsc934936408@sjtu.edu.cn>
2019-07-29 09:08:55 +0800

    add date picker

--- code/frontend/MiniApp_frontend/utils/util.js
+++ code/frontend/MiniApp_frontend/utils/util.js
@@ -9,11 +9,29 @@ const formatTime = date => {
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+const formatBeginDate = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  return [year, month, day].map(formatNumber).join('-')
+}
+
+const formatEndDate = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 2
+  const day = date.getDate()
+
+  return [year, month, day].map(formatNumber).join('-')
+}
+
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatBeginDate: formatBeginDate,
+  formatEndDate: formatEndDate
 }

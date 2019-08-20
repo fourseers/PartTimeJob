fa466e9b602ca5358f1f5179b7d66003a532c1ea
ShenJiahuan <shenjiahuan@sjtu.edu.cn>
2019-08-20 09:53:11 +0000

    fix bug of docker swarm deploy

--- code/backend/deploy.py
+++ code/backend/deploy.py
@@ -11,5 +11,5 @@ def execute(cmd):
 execute("mvn clean package -Dmaven.test.skip=true")
 execute("docker-compose build")
 execute("docker-compose push")
-execute("export .env")
-execute("docker stack deploy -c docker-compose.yml backend")
+execute("docker-compose config > /tmp/preprocessed-docker-compose.yml")
+execute("env $(cat .env | xargs) docker stack deploy -c /tmp/preprocessed-docker-compose.yml backend")
